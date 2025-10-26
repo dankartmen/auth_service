b024f0ec00c2bd99f8616ceb6b64047d9659b4a7
Danil <overlordforever@yandex.ru>
2025-10-26 19:26:55 +0300

    fix: добавлен import init_db в main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.database import init_db
 from app.routers import auth, exercises, history, questionnaires, schedules
 
 app = FastAPI(title="Health App API", version="0.0.1")
